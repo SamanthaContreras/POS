81d2c7c495394b45e910977a5a3817f30a981e73
Samantha Contreras <a291379@uach.mx>
2016-04-22 19:14:30 -0600

    Estructura y funcionamiento

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var $product = $("input[type='text']");
+	var $price = $product.next();
+	var $quantity = $price.next();
+	var btnDelete = "<button class='delete'>Delete</button>";
+	var totalItem = 0;
+	var total = 0;
+
+	$("button").click(function(){
+		totalItem = $price.val() * $quantity.val();
+		var newRow = "<tr><td>"+$product.val()
+					+"</td><td>$"+$price.val()+"</td><td>"
+					+$quantity.val()+"</td><td>"+totalItem+"</td><td>"+btnDelete+"</tr>";
+		$("tbody").append(newRow);
+		
+		$("tfoot tr td").eq(1).text(total+=totalItem);
+		$product.val("");
+		$price.val("");
+		$quantity.val("");
+	});
+	$(document).on("click",".delete",function(){
+		$(this).parentsUntil("tbody").remove();
+	});
+
+});
